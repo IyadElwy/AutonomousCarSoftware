881701a003578a3f97dfe45764feebe3cfafaa8d
Iyad Elwy <iyadelwy1@gmail.com>
2023-03-25 19:00:40 +0200

    Try to enhance remote control functionality

--- MotorControl/motor_controller.py
+++ MotorControl/motor_controller.py
@@ -36,19 +36,3 @@ class MotorDriver:
         GPIO.output(self.In2, GPIO.LOW)
         self.pwm.ChangeDutyCycle(0)
         sleep(slp)
-
-
-        
-
-
-
-# f_b_motor = MotorDriver(25, 23, 24)
-# l_r_motor = MotorDriver(21, 20, 16)
-# 
-# 
-# while True:
-#     f_b_motor.forward(slp=0, speed=100)
-#     l_r_motor.forward(100)
-    
-#     l_r_motor.stop()
-#     f_b_motor.stop()
